41aedb3508ce2e8ea88fb2524173e11d5dd748f4
abcd567a <abcd567@hotmail.com>
2024-10-22 06:31:10 -0400

    Update install-md2.sh

--- install-md2.sh
+++ install-md2.sh
@@ -15,6 +15,7 @@ echo -e "\e[1;32m...INSTALLING DEPENDENCY 1 of 3 (libssl1.1:armhf) ... \e[39m"
 sleep 2
 wget -O ${INSTALL_FOLDER}/libssl1.1_1.1.1w-0+deb11u1_armhf.deb http://http.us.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.1w-0+deb11u1_armhf.deb
 dpkg -i ${INSTALL_FOLDER}/libssl1.1_1.1.1w-0+deb11u1_armhf.deb
+apt -y --fix-broken install
 sudo apt install -y libssl1.1:armhf
 echo -e "\e[1;32m...INSTALLING DEPENDENCY 2 of 3 (libstdc++6:armhf) ... \e[39m"
 sleep 2
